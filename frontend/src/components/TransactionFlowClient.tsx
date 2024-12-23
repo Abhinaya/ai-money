4c05d2d5c6f745ca36c1e2e88030a5cf71aac1ab
Abhinaya Sukumar <abhinaya.sukumar@gmail.com>
2024-12-22 19:10:19 -0800

    loading icon for progress

--- frontend/src/components/TransactionFlowClient.tsx
+++ frontend/src/components/TransactionFlowClient.tsx
@@ -2,6 +2,7 @@
 
 import { useAgentWorkflow } from "@/hooks/useAgentworkflow";
 import { Button } from "@/components/ui/button";
+import { LoadingIcon } from '@/components/LoadingIcon';
 import { useEffect, useState, useRef } from "react";
 import {
   Table,
@@ -12,6 +13,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import TransactionsPage from "@/app/txns/page";
+import { LoadingSpinner } from "./LoadingSpinner";
 
 export function TransactionFlowClient() {
   const [isMounted, setIsMounted] = useState(false);
@@ -99,26 +101,9 @@ export function TransactionFlowClient() {
           <div className="text-xs font-mono float-right">
             Connection Status: {isConnected ? "Connected" : "Disconnected"}
             {isConnected && (
-              <svg
-                className="animate-spin h-5 w-5 text-green-500 inline ml-2"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
-              </svg>
+              <div className="inline-block float-right pl-2">
+                <LoadingIcon width={14} height={14} className="text-green-500" />
+              </div>
             )}
           </div>
           {!isConnected && currentState !== "completed" && (
@@ -148,7 +133,7 @@ export function TransactionFlowClient() {
                 ></div>
               </div>
               <div className="text-sky-600 font-bold pt-5">
-                All transactions: {progress ? progress.total : 'NA'} | Categorized transactions: {progress ? progress.processed : 'NA'}
+                All transactions: {progress ? progress.total : <LoadingIcon width={14} height={14} className="text-sky-600 inline-block" />} | Categorized transactions: {progress ? progress.processed : <LoadingIcon width={14} height={14} className="text-sky-600 inline-block" />}
               </div>
             </div>
           )}
