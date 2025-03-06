a2b2c7708cf7bfeefdeb103126b46e68498467f5
Shristi modi <shristimodi95@gmail.com>
2025-03-06 23:07:49 +0530

    removed extra lines, added a new exception

--- backend/convert_currency_api.py
+++ backend/convert_currency_api.py
@@ -2,16 +2,11 @@ from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 
 router = APIRouter()
-
-
 USD_TO_INR_RATE = 86.84
-
-
 class CurrencyConversionRequest(BaseModel):
-    amount: int
+    amount: float
     from_currency: str
     to_currency: str
-
 
 class CurrencyConversionResponse(BaseModel):
     amount: int
@@ -19,11 +14,16 @@ class CurrencyConversionResponse(BaseModel):
 
 @router.post("/convert-currency", response_model=CurrencyConversionResponse)
 async def convert_currency(request: CurrencyConversionRequest):
-
-    if request.from_currency.upper() != "USD" or request.to_currency.upper() != "INR":
-        raise HTTPException(status_code=500, detail="Only USD to INR conversion is supported.")
+  try:
+        if request.from_currency.upper() != "USD" or request.to_currency.upper() != "INR":
+          raise HTTPException(status_code=500, detail="Only USD to INR conversion is supported.")
     
+        if isinstance(request.amount, float) and not request.amount.is_integer():
+          raise HTTPException(status_code=400, detail="Amount must be a whole number.")
+    
+        converted_amount = int(request.amount * USD_TO_INR_RATE)
 
-    converted_amount = int(request.amount * USD_TO_INR_RATE)
-
-    return {"amount": converted_amount, "currency": "INR"}
+        return {"amount": converted_amount, "currency": "INR"}
+  
+  except HTTPException as e:
+    raise e
